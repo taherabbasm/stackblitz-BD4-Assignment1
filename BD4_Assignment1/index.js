1cc3a6c2babb163e4cc6b2e0dccdaea9e834f37f
taherabbasm <git@stackblitz.com>
2024-11-28 09:16:21 +0530

    Initial commit

---
+++ BD4_Assignment1/index.js
@@ -0,0 +1,203 @@
+const express = require('express');
+const { resolve } = require('path');
+const cors = require('cors');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+const app = express();
+const port = 3010;
+
+let db;
+(async () => {
+  db = await open({
+    filename: './BD4_Assignment1/database.sqlite',
+    driver: sqlite3.Database,
+  });
+})();
+
+// End point 1
+async function fetchAllRestaurants() {
+  let query = 'SELECT * FROM restaurants';
+  let response = await db.all(query);
+  return { restaurants: response };
+}
+app.get('/restaurants', async (req, res) => {
+  try {
+    let results = await fetchAllRestaurants();
+    if (results.restaurants.length === 0) {
+      return res.status(404).json({ message: 'No Restaurants Found' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// End point 2
+async function fetchRestaurantById(id) {
+  let query = 'SELECT * FROM restaurants WHERE id = ?';
+  let response = await db.all(query, [id]);
+  return { restaurants: response };
+}
+app.get('/restaurants/details/:id', async (req, res) => {
+  let id = parseInt(req.params.id);
+  try {
+    let results = await fetchRestaurantById(id);
+    if (results.restaurants.length === 0) {
+      return res
+        .status(400)
+        .json({ Message: 'No Restaurant with Id ' + id + ' Found.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// End point 3
+async function fetchRestaurantsByCuisine(cuisine) {
+  let query = 'SELECT * FROM restaurants WHERE cuisine = ?';
+  let response = await db.all(query, [cuisine]);
+  return { restaurants: response };
+}
+app.get('/restaurants/cuisine/:cuisine', async (req, res) => {
+  let cuisine = req.params.cuisine;
+  try {
+    let results = await fetchRestaurantsByCuisine(cuisine);
+    if (results.restaurants.length === 0) {
+      return res.status(404).json({
+        message: 'No Restaurants with Cuisine ' + cuisine + ' Found.',
+      });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// End point 4
+async function filterRestaurantsByVegSeatingAndLuxury(
+  isVeg,
+  hasOutdoorSeating,
+  isLuxury
+) {
+  let query =
+    'SELECT * FROM restaurants WHERE isVeg = ? AND hasOutdoorSeating = ? AND isLuxury = ?';
+  let response = await db.all(query, [isVeg, hasOutdoorSeating, isLuxury]);
+  return { restaurants: response };
+}
+app.get('/restaurants/filter', async (req, res) => {
+  let isVeg = req.query.isVeg;
+  let hasOutdoorSeating = req.query.hasOutdoorSeating;
+  let isLuxury = req.query.isLuxury;
+  try {
+    let results = await filterRestaurantsByVegSeatingAndLuxury(
+      isVeg,
+      hasOutdoorSeating,
+      isLuxury
+    );
+    if (results.restaurants.length === 0) {
+      return res
+        .status(404)
+        .json({ message: 'No Restaurants With Filters Found.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// End point 5
+async function fetchByRatingDescending() {
+  let query = 'SELECT * FROM restaurants ORDER BY rating DESC';
+  let response = await db.all(query);
+  return { restaurants: response };
+}
+app.get('/restaurants/sort-by-rating', async (req, res) => {
+  try {
+    let results = await fetchByRatingDescending();
+    if (results.restaurants.length === 0) {
+      return res.status(404).json({ message: 'No Restaurants Found.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// End point 6
+async function fetchAllDishes() {
+  let query = 'SELECT * FROM dishes';
+  let response = await db.all(query);
+  return { dishes: response };
+}
+app.get('/dishes', async (req, res) => {
+  try {
+    let results = await fetchAllDishes();
+    if (results.dishes.length === 0) {
+      return res.status(404).json({ message: 'No Dishes Found.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// End point 7
+async function fetchDishesById(id) {
+  let query = 'SELECT * FROM dishes WHERE id = ?';
+  let response = await db.all(query, [id]);
+  return { dishes: response };
+}
+app.get('/dishes/details/:id', async (req, res) => {
+  let id = req.params.id;
+  try {
+    let results = await fetchDishesById(id);
+    if (results.dishes.length === 0) {
+      return res.status(404).json({ message: 'No dish found with id ' + id });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// End point 8
+async function filterDishesByVeg(isVeg) {
+  let query = 'SELECT * FROM dishes WHERE isVeg = ?';
+  let response = await db.all(query, [isVeg]);
+  return { dishes: response };
+}
+app.get('/dishes/filter', async (req, res) => {
+  let isVeg = req.query.isVeg.toString();
+  try {
+    let results = await filterDishesByVeg(isVeg);
+    if (results.dishes.length === 0) {
+      return res.status(404).json({ message: 'No Dishes Found.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// End point 9
+async function fetchDishesSortByPriceAscending() {
+  let query = 'SELECT * FROM dishes ORDER BY price';
+  let response = await db.all(query);
+  return { dishes: response };
+}
+app.get('/dishes/sort-by-price', async (req, res) => {
+  try {
+    let results = await fetchDishesSortByPriceAscending();
+    if (results.dishes.length === 0) {
+      return res.status(404).json({ message: 'No Dishes Found.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
